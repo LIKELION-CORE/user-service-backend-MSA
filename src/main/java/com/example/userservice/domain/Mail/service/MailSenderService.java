419e392b1196b3371afa6703eaad06d38cdf3da7
agape1225 <scg9268@gmail.com>
2023-10-12 10:20:25 +0900

    ✨ create mail service
    - create mail service
    - add mail template

--- src/main/java/com/example/userservice/domain/Mail/service/MailSenderService.java
+++ src/main/java/com/example/userservice/domain/Mail/service/MailSenderService.java
@@ -2,15 +2,15 @@ package com.example.userservice.domain.Mail.service;
 
 import com.example.userservice.domain.Mail.dto.PasswordChangeMailDto;
 import com.example.userservice.domain.Mail.dto.VerificationMailDto;
-import org.springframework.messaging.MessagingException;
 
+import javax.mail.MessagingException;
 import java.util.List;
 
 public interface MailSenderService {
     //임시 비밀번호 전송 메소드
     void sendNewPasswordMail(PasswordChangeMailDto passwordChangeMailDto) throws MessagingException;
     //인증 번호 전송 메소드
-    void sendVerificationMail(VerificationMailDto verificationMailDto) throws MessagingException;
+    void sendVerificationMail(VerificationMailDto verificationMailDto) throws MessagingException, javax.mail.MessagingException;
 
     //이건 일단 무시해주세요!
     //void sendFailMail(List<AcceptEmailDto> acceptEmailDto) throws MessagingException;
